6c11b3b03655c54db07cd5dfd87b2aac42eae701
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-01-22 09:12:35 +0000

    pycharm

--- first.py
+++ first.py
@@ -1,3 +1,25 @@
-print("hello")
-a= 12
+path = 'C://Users//vdi-student//Desktop//rep//rozliczenie.csv'
+mode = 'r'
 
+with open(path, mode) as plik_csv:
+    content = plik_csv.readlines()
+
+for i in range(len(content)):
+    content[i] = content[i].replace('\n', '')
+    content[i] = content[i].split(',')
+
+print(content)
+
+total = 0
+for i in range(1,len(content)):
+    total +=int(content[i][1])
+avg = total/(len(content)-1)
+print(round(avg))
+
+#macierzynski
+total = 0
+for i in range(1,len(content)):
+    if content[i][4] == 't' and content[i][3] == 'k':
+        total +=1
+
+print(total)
