1b1b6bb2e342a58a3b5377ac4ba69a33d8ca639b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-01-22 12:51:04 +0000

    cwiczenia z funkcji

--- czysty.py
+++ czysty.py
@@ -2,3 +2,16 @@ from funkcje import *
 
 def test_add():
     assert add(3,5) == 8
+    assert add(120,43) == 163
+
+def test_multiply():
+    assert multiply(7,7) == 49
+
+def test_fizzbuzz():
+    assert fizzbuzz(1) ==1
+    assert fizzbuzz(14)== 14
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(30) == "FizzBuzz"
+    assert fizzbuzz(-7) == "Number out of range"
+    assert fizzbuzz (4.62) == "Buzz"
--- funkcje.py
+++ funkcje.py
@@ -1,2 +1,20 @@
 def add(x,y):
     return x+y
+
+def multiply(x,y):
+    return x*y
+
+def fizzbuzz(i):
+    if i>0:
+        i = round(i)
+        if i%3 ==0 and i%5 == 0:
+            return "FizzBuzz"
+        elif i%3== 0:
+            return "Fizz"
+        elif i%5 == 0:
+            return "Buzz"
+        else:
+            return i
+    else:
+        return "Number out of range"
+
